a6dc13e09f0a5d57ee75139a2e8b2fbb0b4d2e5c
AntonioLeitaoJr <leitaoint@gmail.com>
2025-04-27 12:13:36 -0300

    Corrige método de autenticação para usar Secrets no Streamlit

--- paginas/servico_sheets.py
+++ paginas/servico_sheets.py
@@ -1,21 +1,16 @@
 import gspread
+import json
+import streamlit as st
 from google.oauth2.service_account import Credentials
 
-# Caminho da sua chave JSON
-CAMINHO_CHAVE = "paginas/GOOGLESHEETS/guiaairbnbrespostas-fdad952e267a.json"
-
-# Escopos necessários para edição do Google Sheets
 ESCOPO = ["https://www.googleapis.com/auth/spreadsheets"]
 
-# Criação das credenciais
-credenciais = Credentials.from_service_account_file(CAMINHO_CHAVE, scopes=ESCOPO)
+info = json.loads(st.secrets["GOOGLE_SHEETS_CREDENTIALS"])
+credenciais = Credentials.from_service_account_info(info, scopes=ESCOPO)
 
-# Autenticação com gspread
 cliente = gspread.authorize(credenciais)
 
-# Abra a planilha pelo ID (copiado da URL do Google Sheets)
 PLANILHA_ID = "1WNAVAuX0OykxQWScj7s5I7gWGBaWlyn8uceYy8wpuak"
 planilha = cliente.open_by_key(PLANILHA_ID)
 
-# Seleciona a primeira aba da planilha
 aba = planilha.sheet1
